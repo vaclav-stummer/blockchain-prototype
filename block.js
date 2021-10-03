0816b846b603fbb6da4029b008bca4e45bdd8693
Vaclav Stummer <vaclav@oakslab.com>
2021-10-03 15:43:00 +0200

    hashing addition

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp = timestamp;
@@ -6,12 +8,30 @@ class Block {
     this.data = data;
   }
 
-  toString() {
+  // Debugging helper function
+  getBlockBump() {
     return `Block - 
         Timestamp: ${this.timestamp}
         Last Hash: ${this.lastHash.substring(0, 10)}
         Hash     : ${this.hash.substring(0, 10)}
         Data     : ${this.data}`;
+  }
+
+  // Static function for initial block of the chain with dummy data
+  static genesis() {
+    return new this("Genesis time", "---", "f1r57-h45h", []);
+  }
+
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 }
 
