90ff5017aa574132c2ee8106f437c63d43882b23
Vaclav Stummer <vaclav@oakslab.com>
2021-10-03 16:13:11 +0200

    blockchain class addition

---
+++ blockchain.test.js
@@ -0,0 +1,22 @@
+const Block = require("./block");
+const Blockchain = require("./blockchain");
+
+describe("Blockchain", () => {
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
+
+  it("start with genesis block first", () => {
+    expect(blockchain.chain[0]).toEqual(Block.genesis());
+  });
+
+  it("adds a new block", () => {
+    const data = "foo";
+
+    blockchain.addBlock(data);
+
+    expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+  });
+});
